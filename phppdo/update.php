a10f1b047f782b2b88ec167e6bd03a3c4bd364b8
krbyjnnn <k.palconete.559180@umindanao.edu.ph>
2026-03-03 09:30:54 +0800

    second commit

--- phppdo/update.php
+++ phppdo/update.php
@@ -5,9 +5,16 @@ if (isset($_POST['update'])) {
     $users_id = $_POST['users_id'];
     $name = $_POST['name'];
     $email = $_POST['email'];
+    $product = $_POST['product'];
+    $amount = $_POST['amount'];
 
-    $stmt = $pdo->prepare("UPDATE users SET name = ?, email = ? WHERE users_id = ?");
-    $stmt ->execute([$name]);
+    $stmt1 = $pdo->prepare("UPDATE users SET name = ?, email = ? WHERE users_id = ?");
+    $stmt1->execute([$name, $email, $users_id]);
+
+    $stmt2 = $pdo->prepare("UPDATE orders SET product = ?, amount = ? WHERE users_id = ?");
+    $stmt2->execute([$product, $amount, $users_id]);
+
+    header("Location: landing.php");
+    exit;
 }
-
 ?>
